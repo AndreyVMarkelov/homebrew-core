aad15e9c9ccfdafc02445d34635becf960849520
Sean Molenaar <SMillerDev@users.noreply.github.com>
2026-06-16 10:36:45 +0200

    Apply suggestion from @SMillerDev

--- Formula/f/fmt.rb
+++ Formula/f/fmt.rb
@@ -5,7 +5,7 @@ class Fmt < Formula
   sha256 "a2f4a8d51178f954e4c339007f77edd76ba0cb2e36f87a48e5a5403d9be5878f"
   license "MIT"
   compatibility_version 1
-  head "https://github.com/fmtlib/fmt.git", branch: "master"
+  head "https://github.com/fmtlib/fmt.git", branch: "main"
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "008ec3a1b84d5db3334383279f6aaf1da457a1173efb14e9bd1d41beb7f6d6da"
